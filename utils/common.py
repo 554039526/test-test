0320a39439c744c300b4975d13d2c70eee40e37f
554039526 <wwlwwlqaz@163.com>
2021-11-26 17:21:35 +0800

    update common

--- utils/common.py
+++ utils/common.py
@@ -5,39 +5,44 @@
     @Time: 2021/11/18 10:56 下午
     @Author: Wan Wenlong
 """
-import re
 from mimesis import Person, Business
 from mimesis.enums import Gender
 from mimesis.schema import Schema
 import random
+from utils.database import select_respIsList
+from utils.util import getOptionValue
+from faker import Faker
+
 
 phone = ['13900000000', '13900000001']
 p = Person('zh')
 b = Business('zh')
+fake = Faker('zh_CN')
 
 
-def get_phone(business, unique=True, i=1):
+def get_phone(business: str, unique=True):
     """
     生成对应业务的手机号
-    :param business: 业务
+    :param business: 业务， 只能传 company、agent
     :param unique: 是否唯一
     :return:
     """
 
-    # res = p.telephone(mask='13#########')
-    phone_tel = '1390000000' + str(i)
-    if unique:
-        if phone_tel in phone:
-            i += 1
-            return get_phone(business, unique=True, i=i)
+    phone_tel = p.telephone(mask='13#########')  # 生成随机手机号
+
+    if unique:  # 是否唯一
+        if business == 'agent':
+            query = getOptionValue('sql', 'select_agent_mobile')
+        elif business == 'company':
+            query = getOptionValue('sql', 'select_admin_mobile')
+        all_phone = select_respIsList(query)   # 取数据库中已有的手机号，type：list
+
+        if phone_tel in all_phone:  # 判断数据库已存在该手机号则重新生成
+            return get_phone(business, unique=True)
         else:
-            print(phone_tel)
             return phone_tel
     else:
         return phone_tel
-
-
-print(get_phone(1, unique=True, i=1))
 
 
 def get_person_name(sex='Female'):
@@ -53,30 +58,54 @@ def get_person_name(sex='Female'):
     return name
 
 
-# print(get_person_name())
-company = ['集团1', '集团2']
-
-
-def get_company_name(unique=False, i=1):
+def get_company_name(unique=False):
     """
     返回公司的名称
     :return:
     """
-    # res = b.company() + b.company_type()
-    res = '集团' + str(i)
-    if unique:
-        if res in company:
-            i += 1
-            return get_company_name(True, i)
+    res = b.company() + b.company_type()  # 随机生成公司名
+
+    if unique:  # 是否唯一
+        query = getOptionValue('sql', 'select_office_name')
+        all_name = select_respIsList(query)
+        if res in all_name:
+            return get_company_name(True)
         else:
             return res
     else:
         return res
 
 
-a = get_company_name(unique=True, i=1)
-print(a)
+def get_sms_code(mobile: str, rand=False):
+    """
+    获取手机号对应的验证码,
+    rand = False, 随机获取一个6位数字，用于错误的验证码
+    :param mobile:
+    :param rand:
+    :return:
+    """
+    if rand:
+        res = random.randint(100000, 999999)
+    else:
+        query = getOptionValue('sql', 'select_sms_by_mobile').replace('ins', mobile)
+        sms_code = select_respIsList(query)
+        res = sms_code[0]
+    return res
 
 
-
-
+def get_id(unique=True):
+    """
+    获取随机的身份证号码
+    :param unique:
+    :return:
+    """
+    id = fake.ssn()
+    if unique:
+        query = getOptionValue('sql', 'select_id')
+        all_ids = select_respIsList(query)
+        if id in all_ids:
+            return get_id(True)
+        else:
+            return id
+    else:
+        return id
--- utils/database.py
+++ utils/database.py
@@ -12,20 +12,47 @@ from utils.util import getItemsSection
 database_info = getItemsSection('sqlserver')
 
 
-def select(query):
+def select_respIsList(query):
+    """
+    调用：select(query='select list_master_id from list_listing_mster ')
+    结果以 list 返回: ['222222222', '18703651000', '15981812162', '18703651002']
+    :param query: 例如：
+    :return:
+    """
+    res_list = []
+    try:
+        with pymssql.connect(server=database_info['host'], port=database_info['port'], database=database_info['database'],
+                             user=database_info['user'], password=database_info['password']) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(query)
+                for row in cursor:
+                    res_list.append(row[0])
+                return res_list
+    except Exception as error:
+        print(f'Database error: {error}')
+
+
+def select_respIsDict(query):
+    """
+    调用：select(query='select list_master_id from list_listing_mster ')
+    结果以 list[dict] 返回: [{'agent_mobile': '222222222'}, {'agent_mobile': '18703651000'}]
+    :param query: 例如：
+    :return:
+    """
     try:
         with pymssql.connect(server=database_info['host'], port=database_info['port'], database=database_info['database'],
                              user=database_info['user'], password=database_info['password']) as conn:
             with conn.cursor(as_dict=True) as cursor:
                 cursor.execute(query)
                 res = cursor.fetchall()
+
                 return res
     except Exception as error:
         print(f'Database error: {error}')
 
 
 if __name__ == '__main__':
-    res = select('select list_master_id from list_listing_master where listing_no = 404559091055210496')
+    res = select_respIsDict('select * from base_agent')
     print(res)
 
 
